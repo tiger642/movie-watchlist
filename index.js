e0c4b42f5d01e6af96a5468f4e306966272f02e3
tiger642 <prbowles@hotmail.com>
2022-08-28 19:35:03 -0500

    updated

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//to-do list
+//save returned api data to local storage
+//add the watchlist button 
+//display the plot and actors
+//display default image for when image is not available
+//add a display more button for the extended plot
+
 //api key
 const apikey = '98e1174'
 
@@ -6,7 +13,10 @@ const searchInput = document.getElementById('input-el')
 const searchBtn = document.getElementById('button-el')
 const movieList = document.getElementById('movies')
 
+//determine whether to display add or remove button 
+let addRemoveWatchlist = ''
 
+//fetch api data if search input is truthy
 async function fetchMovies(searchTerm) {
     const URL = `http://www.omdbapi.com/?apikey=${apikey}&s=${searchTerm}`
     const res = await fetch(`${URL}`)
@@ -23,30 +33,42 @@ async function fetchMovies(searchTerm) {
     }
 }
 
+async function fetchMoreInfo(movieId) {
+    const URL = `http://www.omdbapi.com/?apikey=${apikey}&i=${movieId}`
+    const res= await fetch(`${URL}`)
+    const moreInfo = await res.json()
+    console.log(moreInfo)
+}
+
+//loop through the api data array and display in html
 function displayMovieList(movies) {
     movieList.innerHTML = ''
     let movieCard = document.createElement('div')
     for (let i = 0; i < movies.length; i++) { 
+        fetchMoreInfo(movies[i].imdbID)
         movieCard = `
                     <section class='movie-card'>
                         <img src='${movies[i].Poster}' class='movie-img' />
                         <div class='movie-info'>
                             <div class='movie-title'>${movies[i].Title}</div>
-                            <div>${movies[i].Year}</div>
+                            <div class='movie-year'>${movies[i].Year}</div>
+                            
+                        </div>
+                        <div>
+                            ${addRemoveWatchlist}
                         </div>
                     </section>
                     <hr>
                     `       
         movieList.innerHTML += movieCard
-
-        onmouseover = (moreInfo) => {  }
     }
 }
 
+//grab search bar input
 function findMovies() {
     let searchTerm = searchInput.value
     fetchMovies(searchTerm)
 }
 
-
+//when search field receives inputs fetch api data
 searchInput.addEventListener('input', findMovies)
