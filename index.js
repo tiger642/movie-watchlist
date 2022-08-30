1f9628b957c6820251332c7d36c212fe02f86e94
tiger642 <prbowles@hotmail.com>
2022-08-29 21:22:19 -0500

    finished populating each movie card

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 //to-do list
 //save returned api data to local storage
 //add the watchlist button 
-//display the plot and actors
 //display default image for when image is not available
-//add a display more button for the extended plot
 
 //api key
 const apikey = '98e1174'
@@ -13,15 +11,18 @@ const searchInput = document.getElementById('input-el')
 const searchBtn = document.getElementById('button-el')
 const movieList = document.getElementById('movies')
 
+//array of objects that makes up watchlist
+let watchlistArr = []
+let detailedInfo = []
+
 //determine whether to display add or remove button 
 let addRemoveWatchlist = ''
 
 //fetch api data if search input is truthy
-async function fetchMovies(searchTerm) {
+const fetchMovies = async(searchTerm) => {
     const URL = `http://www.omdbapi.com/?apikey=${apikey}&s=${searchTerm}`
     const res = await fetch(`${URL}`)
     const movies = await res.json()
-    console.log(movies.Search)
     if(movies.Response == 'True') {
         displayMovieList(movies.Search)
     } else {
@@ -33,42 +34,61 @@ async function fetchMovies(searchTerm) {
     }
 }
 
-async function fetchMoreInfo(movieId) {
+const fetchMoreInfo = async(movieId) => {
     const URL = `http://www.omdbapi.com/?apikey=${apikey}&i=${movieId}`
-    const res= await fetch(`${URL}`)
+    const res = await fetch(`${URL}`)
     const moreInfo = await res.json()
     console.log(moreInfo)
+    return moreInfo 
 }
 
 //loop through the api data array and display in html
-function displayMovieList(movies) {
+const displayMovieList = async(movies) => {
     movieList.innerHTML = ''
     let movieCard = document.createElement('div')
     for (let i = 0; i < movies.length; i++) { 
-        fetchMoreInfo(movies[i].imdbID)
-        movieCard = `
-                    <section class='movie-card'>
-                        <img src='${movies[i].Poster}' class='movie-img' />
+        let data = await fetchMoreInfo(movies[i].imdbID)
+        detailedInfo = [data.Poster]
+        movieCard = 
+                    `<section class='movie-card'>
+                        <img src='${data.Poster}' class='movie-img' />
                         <div class='movie-info'>
-                            <div class='movie-title'>${movies[i].Title}</div>
-                            <div class='movie-year'>${movies[i].Year}</div>
-                            
-                        </div>
-                        <div>
-                            ${addRemoveWatchlist}
+                            <section class='section1'>
+                                <div class='movie-title'>${data.Title}</div>
+                                <div class='movie-year'>${data.Year}</div>
+                                <div class='imdb-rating'><i class="fa-solid fa-star"></i>${data.imdbRating}</div>
+                            </section>
+                            <section class='section2'>
+                                <div class='movie-rating'><b>${data.Rated}</b></div>
+                                <div class='movie-runtime'>${data.Runtime}</div>
+                                <button id='watchlist'>Add to Watchlist</button>
+                            </section>
+                            <section class='section3'>
+                                <div class='actors'><b>Actors:</b> ${data.Actors}</div>
+                                <div class='director'><b>Director:</b> ${data.Director}</div>
+                                <div class='plot'>${data.Plot}</div>
+                            </section>
                         </div>
                     </section>
-                    <hr>
-                    `       
+                    <hr>`       
         movieList.innerHTML += movieCard
+        
     }
+    console.log(data.Poster)
 }
 
+
+
 //grab search bar input
-function findMovies() {
+const findMovies = ()  => {
     let searchTerm = searchInput.value
     fetchMovies(searchTerm)
 }
 
-//when search field receives inputs fetch api data
-searchInput.addEventListener('input', findMovies)
+searchBtn.addEventListener('click', findMovies)
+
+searchInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        searchBtn.click()
+    }
+})
